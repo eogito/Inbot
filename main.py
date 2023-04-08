c02060f0a7bb6556855e7a65a6089a4c29446d5a
Felix <felix.zhao2@student.tdsb.on.ca>
2023-04-08 12:36:34 -0400

    HELP

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discord
 import os  # default module
 from dotenv import load_dotenv
 
+# test
+from send_email_test import send_email
+
 load_dotenv()  # load all the variables from the env file
 bot = discord.Bot()
 
@@ -16,8 +19,15 @@ async def hello(ctx):
     await ctx.respond("Hey!")
 
 @bot.slash_command(name="send", description="Send an email")
-async def hello(ctx):
-    await ctx.respond("Hey!")
+async def email(
+        ctx,
+        recipient_email: discord.Option(str, "Enter recipient email", required = True, default = None),
+        subject: discord.Option(str, "Enter subject", required = False, default = None),
+        body: discord.Option(str, "Enter email body", required = False, default = None),
+        attachment: discord.Option(discord.SlashCommandOptionType.FILE, "Add attachments", required = False, default = None)
+    ):
+    send_email(recipient_email, subject, body, )
+    await ctx.send(f"Email sent to {recipient_email} with subject '{subject}'!")
 
 
 bot.run(os.getenv('TOKEN'))  # run the bot with the token
