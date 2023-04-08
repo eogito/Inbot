b8d2eb57b16d204f88f1b225dc352eea7f7ff3e2
Felix <felix.zhao2@student.tdsb.on.ca>
2023-04-07 21:55:01 -0400

    test version of the check email method

---
+++ check_email_test.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+# Date and time
+from datetime import datetime, timezone, timedelta
+# Task scheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+# Gets credentials if user is not already logged in
+def get_credentials():
+    creds = None
+
+    # Check if the token file exists
+    if os.path.exists('token.json'):
+        # Load the saved credentials from the file
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    # If the credentials don't exist or are invalid, prompt the user to log in
+    if not creds or not creds.valid:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+
+        # Save the credentials to the token file for future use
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+# Define a function to check for new emails
+def check_email():
+    now = datetime.now(timezone.utc)
+    one_minute_ago = now - timedelta(minutes=1)
+    query = f'after:{one_minute_ago.strftime("%Y/%m/%d %H:%M:%S")}'
+
+    creds = get_credentials()
+    service = build('gmail', 'v1', credentials=creds)
+
+    try:
+        response = service.users().messages().list(userId='me', q=query).execute()
+        messages = response['messages']
+        print(f'{len(messages)} new messages received since {one_minute_ago}')
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+
+def print_message():
+    print("hi")
+
+# Set up a scheduler to run the email check every minute
+scheduler = BlockingScheduler()
+
+# Test
+creds = get_credentials()
+
+if creds:
+    scheduler.add_job(print_message, 'interval', seconds=1)
+    scheduler.start()
