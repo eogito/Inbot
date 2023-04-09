370cc46107d8273bc540b1f28ada05f82e0d728f
Cqctxs <seanzhao2007@gmail.com>
2023-04-09 09:19:18 -0400

    Tried to get read email to work on bot, still some errors

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import time
+
 import discord
 import os  # default module
+import asyncio
 from dotenv import load_dotenv
 
 # test
 from send_email_test import send_email
+from read_email import get_emails
 
 load_dotenv()  # load all the variables from the env file
 bot = discord.Bot()
-
 
 @bot.event
 async def on_ready():
@@ -18,24 +21,37 @@ async def on_ready():
 async def hello(ctx):
     await ctx.respond("Hey!")
 
+
 @bot.slash_command(name="send", description="Send an email")
 async def email(
         ctx,
         recipient_email: str,
-        subject: discord.Option(str, "Enter subject", required = False, default = None),
-        body: discord.Option(str, "Enter email body", required = False, default = None),
-        attachment: discord.Option(discord.SlashCommandOptionType.attachment, "Add attachments", required = False, default = None)
-    ):
+        subject: discord.Option(str, "Enter subject", required=False, default=None),
+        body: discord.Option(str, "Enter email body", required=False, default=None),
+        attachment: discord.Option(discord.SlashCommandOptionType.attachment, "Add attachments", required=False,
+                                   default=None)
+):
     if send_email(recipient_email, subject, body, attachment):
         await ctx.respond(f"Email sent to {recipient_email} with subject '{subject}'!")
     else:
         await ctx.respond(f"Something went wrong, please try again.")
-    
+
 
 @bot.slash_command(name="test")
 async def command(ctx, file: discord.SlashCommandOptionType.attachment):
     print(str(file))
     await ctx.respond(str(file))
 
+
+@bot.slash_command(name="read", description="Read your Emails")
+async def read(ctx):
+    while True:
+        email_data = get_emails()
+        if email_data:
+            for mail in email_data:
+                await ctx.respond(f"{mail[0]}\n{mail[1]}\n{mail[2]}")
+        else:
+            await ctx.respond("No emails")
+        await asyncio.sleep(10)
 
 bot.run(os.getenv('TOKEN'))  # run the bot with the token
--- read_email.py
+++ read_email.py
@@ -41,52 +41,38 @@ def get_credentials():
 
 
 # Reads all unread emails and marks them as read
-def read_email():
+def get_emails():
+    emails = []
     creds = get_credentials()
     # Call the Gmail API
     service = build('gmail', 'v1', credentials=creds)
     results = service.users().messages().list(maxResults=500, userId='me', labelIds=['INBOX'],
                                               q='is:unread').execute()
 
-    try:
-        messages = results.get('messages', [])
-        if messages:
-            for message in messages[::-1]:
-                msg = service.users().messages().get(userId='me', id=message['id']).execute()
-                # Mark all emails as read
-                service.users().messages().modify(userId='me', id=message['id'],
-                                                  body={'removeLabelIds': ['UNREAD']}).execute()
-                email_data = msg['payload']['headers']
-                for values in email_data:
-                    name = values['name']
-                    if name == "From":
-                        from_name = values['value']
-                        print("You have a new message from: " + from_name)
+    messages = results.get('messages', [])
+    if messages:
+        for message in messages[::-1]:
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            # Mark all emails as read
+            service.users().messages().modify(userId='me', id=message['id'],
+                                              body={'removeLabelIds': ['UNREAD']}).execute()
+            email_data = msg['payload']['headers']
+            for values in email_data:
+                name = values['name']
+                if name == "From":
+                    from_name = values['value']
+                    # print("You have a new message from: " + from_name)
 
-                        # Get email subject
-                        subject = ""
-                        for val in email_data:
-                            if val['name'] == "Subject":
-                                subject = val['value']
-                        if subject == "":
-                            subject = "(No Subject)"
+                    # Get email subject
+                    subject = ""
+                    for val in email_data:
+                        if val['name'] == "Subject":
+                            subject = val['value']
+                    if subject == "":
+                        subject = "(No Subject)"
+                    emails.append(["You have a new message from: " + from_name, "Subject: " + html.unescape(subject),
+                                   html.unescape(msg['snippet'])])
+                    # print("Subject:", html.unescape(subject))
+                    # print(html.unescape(msg['snippet']))
+    return emails
 
-                        print("Subject:", html.unescape(subject))
-                        print(html.unescape(msg['snippet']))
-                        print('\n')
-
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
-
-
-# Set up a scheduler to run the email check every minute
-scheduler = BlockingScheduler()
-
-# Test
-creds = get_credentials()
-service = build('gmail', 'v1', credentials=creds)
-
-if creds:
-    scheduler.add_job(read_email, 'interval', seconds=60)
-    scheduler.start()
