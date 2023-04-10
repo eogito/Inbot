18deb9c837aa14df7e1452182dd383fb42d375da
eogito <106759344+eogito@users.noreply.github.com>
2023-04-09 22:34:42 -0400

    Update read_email.py

--- read_email.py
+++ read_email.py
@@ -3,67 +3,70 @@ from __future__ import print_function
 import html
 import os.path
 
-from credential_check import credential_by_tag
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-# Date and time
-from datetime import datetime, timezone, timedelta
-# Task scheduler
-from apscheduler.schedulers.blocking import BlockingScheduler
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/gmail.readonly',
           'https://www.googleapis.com/auth/gmail.modify', 'https://mail.google.com/']
 
 
+# Gets credentials if user is not already logged in
+def get_credentials():
+    creds = None
+
+    # Check if the token file exists
+    if os.path.exists('token.json'):
+        # Load the saved credentials from the file
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    # If the credentials don't exist or are invalid, prompt the user to log in
+    if not creds or not creds.valid:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+
+        # Save the credentials to the token file for future use
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+
 # Reads all unread emails and marks them as read
-def read_email(uid):
-    creds = credential_by_tag(uid)
+def get_emails():
+    emails = []
+    creds = get_credentials()
     # Call the Gmail API
     service = build('gmail', 'v1', credentials=creds)
     results = service.users().messages().list(maxResults=500, userId='me', labelIds=['INBOX'],
                                               q='is:unread').execute()
 
-    try:
-        messages = results.get('messages', [])
-        if messages:
-            for message in messages[::-1]:
-                msg = service.users().messages().get(userId='me', id=message['id']).execute()
-                # Mark all emails as read
-                service.users().messages().modify(userId='me', id=message['id'],
-                                                  body={'removeLabelIds': ['UNREAD']}).execute()
-                email_data = msg['payload']['headers']
-                for values in email_data:
-                    name = values['name']
-                    if name == "From":
-                        from_name = values['value']
-                        print("You have a new message from: " + from_name)
+    messages = results.get('messages', [])
+    if messages:
+        for message in messages[::-1]:
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            # Mark all emails as read
+            service.users().messages().modify(userId='me', id=message['id'],
+                                              body={'removeLabelIds': ['UNREAD']}).execute()
+            payload = msg['payload']
+            headers = payload['headers']
 
-                        # Get email subject
-                        subject = ""
-                        for val in email_data:
-                            if val['name'] == "Subject":
-                                subject = val['value']
-                        if subject == "":
-                            subject = "(No Subject)"
+            sender = ''
+            subject = ''
 
-                        print("Subject:", html.unescape(subject))
-                        print(html.unescape(msg['snippet']))
-                        print('\n')
+            for header in headers:
+                if header['name'] == 'From':
+                    sender = header['value']
 
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
+            for header in headers:
+                if header['name'] == 'Subject':
+                    subject = header['value']
+            if subject == "":
+                subject = "(No Subject)"
 
-
-# Set up a scheduler to run the email check every minute
-''' 
-scheduler = BlockingScheduler()
-
-if creds:
-    scheduler.add_job(read_email, 'interval', seconds=60)
-    scheduler.start()
-'''
+            body = msg['snippet']
+            emails.append(
+                ["You have a new message from: " + sender, "Subject: " + html.unescape(subject), html.unescape(body) + "..."])
+    return emails
