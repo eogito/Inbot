0da3ea7c4fb346a66415ec689edeca38380dcf09
eogito <106759344+eogito@users.noreply.github.com>
2023-04-09 22:32:10 -0400

    Update and rename send_email_test.py to send_email.py

--- send_email.py
+++ send_email.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 import os.path
-from credential_check import credential_by_tag
+
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -18,9 +18,32 @@ import requests
 SCOPES = ['https://www.googleapis.com/auth/gmail.send', 'https://www.googleapis.com/auth/gmail.readonly',
           'https://www.googleapis.com/auth/gmail.modify', 'https://mail.google.com/']
 
+# Gets credentials if user is not already logged in
 
-def send_email(uid,to, subject=None, body='', attachment=None):
-    creds = credential_by_tag(uid)
+
+def get_credentials():
+    creds = None
+
+    # Check if the token file exists
+    if os.path.exists('token.json'):
+        # Load the saved credentials from the file
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    # If the credentials don't exist or are invalid, prompt the user to log in
+    if not creds or not creds.valid:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+
+        # Save the credentials to the token file for future use
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+
+def send_email(to, subject=None, body='', attachment=None):
+    creds = get_credentials()
     service = build('gmail', 'v1', credentials=creds)
 
     # Create a new email message with attachments
