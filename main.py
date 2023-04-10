9fe8df9d4a89d2c45e93fe091056793cae1b81e9
eogito <106759344+eogito@users.noreply.github.com>
2023-04-09 22:37:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import discord
-import os  # default module
+import os
+import asyncio
 from dotenv import load_dotenv
 
-# test
-from send_email_test import send_email
+from send_email import send_email
+from read_email import get_emails
 
 load_dotenv()  # load all the variables from the env file
 bot = discord.Bot()
+reading = False
 
 
 @bot.event
@@ -14,30 +16,46 @@ async def on_ready():
     print(f"{bot.user} is ready and online!")
 
 
-@bot.slash_command(name="hello", description="Say hello to the bot")
-async def hello(ctx):
-    await ctx.respond("Hey!")
-
 @bot.slash_command(name="send", description="Send an email")
 async def email(
         ctx,
         recipient_email: str,
-        subject: discord.Option(str, "Enter subject", required = False, default = None),
-        body: discord.Option(str, "Enter email body", required = False, default = None),
-        attachment: discord.Option(discord.SlashCommandOptionType.attachment, "Add attachments", required = False, default = None),
-    ):
-    print(str(ctx.author))
-    if send_email(uid=str(ctx.author), to=recipient_email, subject=subject, body=body, attachment=attachment):
-
+        subject: discord.Option(str, "Enter subject", required=False, default=None),
+        body: discord.Option(str, "Enter email body", required=False, default=None),
+        attachment: discord.Option(discord.SlashCommandOptionType.attachment, "Add attachments", required=False,
+                                   default=None)
+):
+    if send_email(recipient_email, subject, body, attachment):
         await ctx.respond(f"Email sent to {recipient_email} with subject '{subject}'!")
     else:
         await ctx.respond(f"Something went wrong, please try again.")
-    
 
-@bot.slash_command(name="test")
-async def command(ctx, file: discord.SlashCommandOptionType.attachment):
-    print(str(file))
-    await ctx.respond(str(file))
+
+@bot.slash_command(name="read", description="Read your Emails")
+async def read(ctx):
+    global reading
+    reading = True
+    await ctx.respond("Reading emails...")
+    while reading:
+        email_data = get_emails()
+        print(email_data)
+        if email_data:
+            for mail in email_data:
+                message = f"{mail[0]}\n{mail[1]}\n\n{mail[2]}"
+                message = "%.2000s" % message
+                await ctx.respond(message)
+        await asyncio.sleep(10)
+
+
+@bot.slash_command(name="stop", description="Stop Reading Emails")
+async def stop(ctx):
+    global reading
+    if reading:
+        reading = False
+        await ctx.respond("Emails have been stopped")
+    else:
+        await ctx.respond("Emails are not currently being read!")
+
 
 
 bot.run(os.getenv('TOKEN'))  # run the bot with the token
