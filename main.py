caacc9aa5be263819aa48ed39a10585ccca8da2b
Cqctxs <seanzhao2007@gmail.com>
2023-04-09 20:09:31 -0400

    Added stop function

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from read_email import get_emails
 
 load_dotenv()  # load all the variables from the env file
 bot = discord.Bot()
+reading = False
 
 @bot.event
 async def on_ready():
@@ -45,8 +46,10 @@ async def command(ctx, file: discord.SlashCommandOptionType.attachment):
 
 @bot.slash_command(name="read", description="Read your Emails")
 async def read(ctx):
+    global reading
+    reading = True
     await ctx.respond("Reading emails...")
-    while True:
+    while reading:
         email_data = get_emails()
         print(email_data)
         if email_data:
@@ -55,5 +58,12 @@ async def read(ctx):
                 message = "%.2000s" % message
                 await ctx.respond(message)
         await asyncio.sleep(10)
+
+
+@bot.slash_command(name="stop", description="Stop Reading Emails")
+async def stop(ctx):
+    global reading
+    reading = False
+    await ctx.respond("Emails have been stopped")
 
 bot.run(os.getenv('TOKEN'))  # run the bot with the token
